55d8e68c838b6bc76caef5e1db41af9c6f85dce2
Namy911 <hodorogeaandrei@gmail.com>
2018-04-02 13:19:40 +0300

    rand answer > 4

--- app/src/main/java/flag/test/com/contryflag/MainActivityFragment.java
+++ app/src/main/java/flag/test/com/contryflag/MainActivityFragment.java
@@ -27,21 +27,18 @@ import java.util.Random;
 
 import flag.test.com.contryflag.model.Country;
 
-public class MainActivityFragment extends Fragment {
+public class MainActivityFragment extends Fragment implements View.OnClickListener {
 
     private static final String JSON_ANSWER = "answer.json";
     private static final String REGION_CAPITAL = "capital";
     private static final String REGION_PHOTO = "photo";
-    private static final String ANSWER_1 = "answer1";
     private static final String BUNDLE_LIST_ANSWERS = "model.bundle.ListAnswer";
+    private static final String BUNDLE_ANSWERS = "fragment.main.answer";
 
     private ImageView countryFlagImg;
     private InputStream stream;
     private TextView txtRegion;
-    private List<Button> answers = new ArrayList<Button>();;
-    private Button answer2;
-    private Button answer3;
-    private Button answer4;
+    private List<Button> buttonList = new ArrayList<Button>();
     private Bitmap bitmapFlag;
 
     private List<Country> countryList;
@@ -49,8 +46,11 @@ public class MainActivityFragment extends Fragment {
     private List<Country> tempList = new ArrayList<>();
     private List<Country> listAnswer = new ArrayList<>();
     private String region;
+    private Country answer;
 
+    private Country bundleAnswer;
     private List<Country> bundleListAnswer = new ArrayList<>();
+
     private static final String TAG = "MainActivityFragment";
 
     @Override
@@ -66,23 +66,22 @@ public class MainActivityFragment extends Fragment {
 
         countryFlagImg = view.findViewById(R.id.contry_flag);
         txtRegion = view.findViewById(R.id.txt_region);
+
         Button button1 = view.findViewById(R.id.answer1);
+        button1.setOnClickListener(this);
         Button button2 = view.findViewById(R.id.answer2);
+        button2.setOnClickListener(this);
         Button button3 = view.findViewById(R.id.answer3);
+        button3.setOnClickListener(this);
         Button button4 = view.findViewById(R.id.answer4);
-        answers.add(button1);
-        answers.add(button2);
-        answers.add(button3);
-        answers.add(button4);
+        button4.setOnClickListener(this);
+        buttonList.add(button1);
+        buttonList.add(button2);
+        buttonList.add(button3);
+        buttonList.add(button4);
 
         init(false);
 
-        answers.get(0).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                init(false);
-            }
-        });
         return view;
     }
 
@@ -90,6 +89,7 @@ public class MainActivityFragment extends Fragment {
     public void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
         outState.putParcelableArrayList(BUNDLE_LIST_ANSWERS, (ArrayList<? extends Parcelable>) listAnswer);
+        outState.putParcelable(BUNDLE_ANSWERS,  answer);
     }
 
     @Override
@@ -97,6 +97,7 @@ public class MainActivityFragment extends Fragment {
         super.onViewStateRestored(savedInstanceState);
         if (savedInstanceState != null){
             bundleListAnswer = savedInstanceState.getParcelableArrayList(BUNDLE_LIST_ANSWERS);
+            bundleAnswer = savedInstanceState.getParcelable(BUNDLE_ANSWERS);
             init(true);
         }
     }
@@ -152,23 +153,16 @@ public class MainActivityFragment extends Fragment {
     private void getRandAnswers(boolean save) {
         if (save){
             listAnswer = bundleListAnswer;
+            answer = bundleAnswer;
         }else {
             listAnswer = setRandAnswers();
+            //answer = selectAnswer();
         }
-
+        //Log.d(TAG, "getRandAnswers:  " + answer.getCountry());
         if (listAnswer.size() > 1) {
-
-            Country answer = listAnswer.get(new Random().nextInt(listAnswer.size()));
-            Log.d(TAG, "setRandAnswers: id answer   " + answer.getCountry());
-
-            for (int i = 0; i < answers.size(); i++) {
-                answers.get(i).setText(listAnswer.get(i).getCountry());
+            for (int i = 0; i < buttonList.size(); i++) {
+                buttonList.get(i).setText(listAnswer.get(i).getCountry());
             }
-//            answers.get().setText(listAnswer.get(0).getCountry());
-//            answer2.setText(listAnswer.get(1).getCountry());
-//            answer3.setText(listAnswer.get(2).getCountry());
-//            answer4.setText(listAnswer.get(3).getCountry());
-            //Log.d(TAG, "getRandAnswers: " + listAnswer.get(3).getCountry());
             try {
                 region = answer.getRegion();
                 stream = getContext().getAssets()
@@ -189,6 +183,10 @@ public class MainActivityFragment extends Fragment {
         }
     }
 
+    private Country selectAnswer(){
+        return listAnswer.get(new Random().nextInt(listAnswer.size()));
+    }
+
     private List<Country> setRandAnswers() {
         List<Country> tempAnswers = new ArrayList<>();
         if (tempList.size() > 1 ){
@@ -205,7 +203,10 @@ public class MainActivityFragment extends Fragment {
         } else if (tempList.size() > 1) {
             tempAnswers = setRandAnswers(tempLite, 1);
         } else {
-            answers.get(0).setEnabled(false);
+            buttonList.get(0).setEnabled(false);
+            buttonList.get(1).setEnabled(false);
+            buttonList.get(2).setEnabled(false);
+            buttonList.get(3).setEnabled(false);
             Log.d(TAG, "--------------------------- Finis -----------------------------------------");
         }
         return tempAnswers;
@@ -221,13 +222,27 @@ public class MainActivityFragment extends Fragment {
             list.remove(list.get(index.get(i)));
         }
 
-        for (int i = 0; i < 3; i++) {
-            list.add(answers.get(i));
+
+//        for (int i = 0; i < 3; i++) {
+//            list.add(answers.get(i));
+//        }
+        int id = new Random().nextInt(answers.size());
+
+        for (int i = 0; i < 4; i++) {
+            if (id == i){
+                Log.d(TAG, "setRandAnswers: =================" + answers.get(id).getCountry()  + "  --  " + i);
+                continue;
+            }else {
+                list.add(answers.get(i));
+                Log.d(TAG, "setRandAnswers else :  " + answers.get(i).getCountry() + "  --  " + i);
+            }
         }
 
         switch (length) {
             case 4:
-                removeCountry(answers.get((3)));
+                answer = answers.get(id);
+                removeCountry(answer);
+                //Log.d(TAG, "setRandAnswers: " + answer.getCountry());
                 break;
             case 3:
                 answers = setAnswerHelper(answers, 2);
@@ -245,6 +260,7 @@ public class MainActivityFragment extends Fragment {
     private List<Country> setAnswerHelper(List<Country> answers, int index){
         answers.add(tempList.get(index));
         removeCountry(tempList.get(index));
+        Log.d(TAG, "setAnswerHelper -- Delete: " + tempList.size() + " / " + tempLite.size() + " ---------------- " + tempList.get(index).getCountry());
         return  answers;
     }
 
@@ -252,6 +268,7 @@ public class MainActivityFragment extends Fragment {
         if (countryList.size() > 1) {
             countryList.remove(country);
             tempLite.add(country);
+            Log.d(TAG, "setAnswerHelper Delete  : " + tempLite.size() + " / " + countryList.size() );
         } else {
             Log.d(TAG, "removeCountry: no deleted");
         }
@@ -261,5 +278,20 @@ public class MainActivityFragment extends Fragment {
         getRandAnswers(save);
         txtRegion.setText(region);
         countryFlagImg.setImageBitmap(bitmapFlag);
+    }
+
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+        if (id == R.id.answer1 || id == R.id.answer2 || id == R.id.answer3 || id == R.id.answer4){
+            Button button = (Button) v;
+            String text = button.getText().toString();
+            if (text.equals(answer.getCountry())){
+                init(false);
+               // Log.d(TAG, "onClick: "+" Answerrrrrrrrrrrrrrrrrr");
+            }else {
+                Log.d(TAG, "onClick: " + "Incorect!!!!!!!!!!!");
+            }
+        }
     }
 }
